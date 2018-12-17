f8285acd7c17d45b7921f729eb0acce1c54f7324
Hung Wei Lin <linhw1978@gmail.com>
2018-12-18 07:39:07 +0800

    adding run.py and modified app.py

--- app.py
+++ app.py
@@ -14,10 +14,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False #disables flask_sqlalchemy's
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Store, '/store/<string:name>')
